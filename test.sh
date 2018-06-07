1ac9610c95d2bacaac328f0c5482baa60fd389d1
Jan Mojzis <jan.mojzis@gmail.com>
2018-06-07 11:34:43 +0200

    test.sh log to stderr

--- test.sh
+++ test.sh
@@ -8,9 +8,9 @@ for i in 65 165 1065 10065; do
   ./mcencrypt 4<pk <data >data.mc8
   ./mcdecrypt 8<sk <data.mc8 >data.new
   if [ "`shasum < data`" = "`shasum < data.new`" ]; then
-    echo "test ${i} ok"
+    echo "test ${i} ok" >&2
   else
-    echo "test ${i} failed"
+    echo "test ${i} failed" >&2
     exit 1
   fi
 done
@@ -18,20 +18,20 @@ done
 #bad secret-key test
 ./mckeypair 5>pk2 9>sk2
 if ./mcdecrypt 8<sk2 <data.mc8 >data.new 2>log; then
-  echo "bad secret-key test failed:"
-  cat log
+  echo "bad secret-key test failed:" >&2
+  cat log >&2
   exit
 else
-  echo "bad secret-key test ok"
+  echo "bad secret-key test ok" >&2
 fi
 
 #short ciphertext test
 if ./mcdecrypt 8<sk </dev/null >data.new 2>log; then
-  echo "short ciphertext test failed:"
-  cat log
+  echo "short ciphertext test failed:" >&2
+  cat log >&2
   exit 1
 else
-  echo "short ciphertext test ok"
+  echo "short ciphertext test ok" >&2
 fi
 
 exit 0
