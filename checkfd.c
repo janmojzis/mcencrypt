3a4885732caf864c97f42a72a0c37685373e596e
Jan Mojzis <jan.mojzis@gmail.com>
2018-06-07 18:32:29 +0200

    checkfd.c revent fix

--- checkfd.c
+++ checkfd.c
@@ -1,3 +1,5 @@
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <errno.h>
 #include "checkfd.h"
@@ -7,6 +9,9 @@ int checkfd(int fd, short event) {
     struct pollfd p;
     char ch = 0;
     int r;
+    struct stat st;
+
+    if (fstat(fd, &st) != 0) return 1;
 
     p.fd = fd;
     p.events = event;
@@ -20,10 +25,8 @@ int checkfd(int fd, short event) {
         if (r != 1) return 0;
         if (r == 1) break;
     }
-    if (p.revents != event) {
-        if (event == POLLOUT) write(fd, &ch, 1);
-        if (event == POLLIN) read(fd, &ch, 1);
-        return 0;
-    }
-    return 1;
+    if (p.revents) return 1;
+    if (event == POLLOUT) write(fd, &ch, 1);
+    if (event == POLLIN) read(fd, &ch, 1);
+    return 0;
 }
